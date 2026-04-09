c6b9b315ac6c6ec391fdc9c8a250dc4bba47e2d6
Vitor Augusto <vitotaugusto1@gmail.com>
2026-04-08 21:36:45 -0300

    FEAT: IMPLEMENTAÇÃO DO CRUD COMPLETO.

--- src/main/java/com/example/meuApp/controller/ProductController.java
+++ src/main/java/com/example/meuApp/controller/ProductController.java
@@ -1,11 +1,15 @@
 package com.example.meuApp.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.example.meuApp.service.ProductService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.meuApp.model.carro;
 
 @Controller
 public class ProductController {
@@ -18,5 +22,32 @@ public class ProductController {
         model.addAttribute("productsList", productService.getAllProducts());
         return "carros/index";
     }
+
+    @GetMapping("/carro/create")
+    public String create(Model model) {
+        model.addAttribute("product", new carro());
+        return "carros/create";
+    }
+
+    @PostMapping("/carro/save")
+    public String postMethodName(@ModelAttribute("product") carro product) {
+        productService.saveProduct(product);
+        return "redirect:/carro";
+
+    }
+
+     @GetMapping("/carro/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        this.productService.deleteProductById(id);
+        return "redirect:/carro";
+    }
+
+   @GetMapping("/carro/edit/{id}")
+    public String edit(@PathVariable Long id, Model model) {
+        carro product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        return "carros/edit";
+    }
+
 
 }
